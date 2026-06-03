bb7c15f1a8d56f7340c42a8ed466ea2c41c8c9c9
Nuona Liang <lnuo026@aucklanduni.ac.nz>
2026-06-03 20:13:27 +1200

    feat: make /health endpoint public

--- todolist/src/health/health.controller.ts
+++ todolist/src/health/health.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get} from "@nestjs/common";
 import { HealthCheck, HealthCheckService } from "@nestjs/terminus";
+import { Public } from "src/common/decorators/public.decorator";
 
 // 把这个控制器挂到 /health 路由前缀。
 @Controller("health")
@@ -9,6 +10,7 @@ export class HealthController{
      // 处理 GET /health
      @Get()
      // 告诉 Terminus 这是一个健康检查端点。
+     @Public()
      @HealthCheck()
      // 执行检查。这里是空数组，表示“只做基本健康检查”。如果要检查 MongoDB/Redis，就往数组里加检查函数。
      check(){
